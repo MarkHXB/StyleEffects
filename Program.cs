2af33586323f19b689f6e239f471bbd987ee72d0
Márk Bakonyi <bakonyimark8@gmail.com>
2021-01-23 20:58:34 +0100

    After created the project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
